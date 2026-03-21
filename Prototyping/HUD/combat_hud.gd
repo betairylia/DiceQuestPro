6580f15802e0fb23cf926ec4e691b1d586c8b93a
betairylia <betairya@gmail.com>
2026-03-21 19:23:01 +0900

    Checkpoint before reroll energy implementation

--- Prototyping/HUD/combat_hud.gd
+++ Prototyping/HUD/combat_hud.gd
@@ -4,6 +4,9 @@ signal reroll
 
 @onready var player_spells: SpellsDetailPanel = $PlayerSpells
 @onready var enemy_spells: SpellsDetailPanel = $EnemySpells
+@onready var spell_name_label: RichTextLabel = $SpellName
+@onready var spell_anno_label: RichTextLabel = $SpellAnnotation
+@onready var phase_anno_label: RichTextLabel = $PhaseAnnotation
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -33,3 +36,32 @@ func _on_prototype_player_spell_updated(spells: Array[MatchedSpell]) -> void:
 
 func _on_prototype_enemy_spell_updated(spells: Array[MatchedSpell]) -> void:
 	enemy_spells.populate(spells)
+
+
+func _on_prototype_phase_entered(phase: Combat.CombatExecPhase) -> void:
+	if phase == Combat.CombatExecPhase.Preparation:
+		spell_name_label.text = ""
+		spell_anno_label.text = ""
+		phase_anno_label.text = ""
+	elif phase == Combat.CombatExecPhase.PlayerRegular:
+		spell_name_label.text = "普通攻击"
+		spell_anno_label.text = ""
+		phase_anno_label.text = "- PLAYER -"
+	elif phase == Combat.CombatExecPhase.EnemyRegular:
+		spell_name_label.text = "普通攻击"
+		spell_anno_label.text = ""
+		phase_anno_label.text = "- ENEMY -"
+	elif phase == Combat.CombatExecPhase.PlayerSpells:
+		spell_name_label.text = ""
+		spell_anno_label.text = ""
+		phase_anno_label.text = "- PLAYER -"
+	elif phase == Combat.CombatExecPhase.EnemySpells:
+		spell_name_label.text = ""
+		spell_anno_label.text = ""
+		phase_anno_label.text = "- ENEMY -"
+
+
+func _on_prototype_spell_triggered(spell: MatchedSpell) -> void:
+	print(spell.spell.display_name)
+	spell_name_label.text = spell.spell.display_name
+	spell_anno_label.text = ""
--- Prototyping/combat.gd
+++ Prototyping/combat.gd
@@ -1,7 +1,21 @@
 extends Node
+class_name Combat
+
+enum CombatExecPhase {
+	PlayerRegular,
+	PlayerSpells,
+	EnemyRegular,
+	EnemySpells,
+	Preparation
+}
+
+# --- Combat schedule ---
+signal phase_entered(phase: CombatExecPhase)
+signal spell_triggered(spell: MatchedSpell)
 
 signal player_spell_updated(spells: Array[MatchedSpell])
 signal enemy_spell_updated(spells: Array[MatchedSpell])
+signal reroll_energy_updated(rerolls: int)
 
 @onready var players: Array[Mob] = [$"PlayerChar-Combat", $"PlayerChar-Combat2", $"PlayerChar-Combat3"]
 @onready var enemies: Array[Mob] = [$EnemySlot1]
@@ -19,6 +33,9 @@ var _player_results: Array[DiceResult] = []
 var _player_matched_spells: Array[MatchedSpell] = []
 var _enemy_matched_spells: Array[MatchedSpell] = []
 
+var _phase: CombatExecPhase = CombatExecPhase.Preparation
+var _rerolls: int = 3
+
 
 func _ready() -> void:
 	for i in range(3):
@@ -26,6 +43,12 @@ func _ready() -> void:
 	enemies[0].setup(enemiesData[0])
 
 	_turn()
+
+
+func _enter_phase(phase: CombatExecPhase) -> void:
+	_phase = phase
+	phase_entered.emit(phase)
+
 
 func _turn() -> void:
 	# Rebuild dice lists and sync state_entered connections.
@@ -39,6 +62,8 @@ func _turn() -> void:
 			die.state_entered.connect(_update_reroll_button)
 	_player_dice = new_player_dice
 	_enemy_dice = _collect_dice(enemies)
+
+	_enter_phase(CombatExecPhase.Preparation)
 
 	await _roll_enemies()
 	await _roll_players()
@@ -67,15 +92,14 @@ func _update_reroll_button(_s) -> void:
 
 
 func _regular_attack(froms: Array[Mob], tos: Array[Mob], from_dice: Array[DiceResult]) -> void:
-	var target = tos[0]
 	await BindGroupAwait.all(Arr.emap(from_dice,
 		func(i: int, die: DiceResult):
 			return die.node.Attack.bind(tos, i * 0.15)
 	))
 
 func _resolve_spells(froms: Array[Mob], tos: Array[Mob], from_spells: Array[MatchedSpell]) -> void:
-	var target = tos[0]
 	for spell in from_spells:
+		spell_triggered.emit(spell)
 		await BindGroupAwait.all(Arr.emap(Arr.unique(spell.matched_dice),
 			func(i: int, die: DiceResult):
 				return die.node.Attack.bind(tos, i * 0.15)
@@ -83,10 +107,16 @@ func _resolve_spells(froms: Array[Mob], tos: Array[Mob], from_spells: Array[Matc
 
 
 func _on_combat_hud_act() -> void:
+
 	for die in _player_dice:
 		die.SetState(RollableDice.DiceCombatState.Determined)
+
+	_enter_phase(CombatExecPhase.PlayerRegular)
 	await _regular_attack(players, enemies, _player_results)
+
+	_enter_phase(CombatExecPhase.PlayerSpells)
 	await _resolve_spells(players, enemies, _player_matched_spells)
+
 	_turn()
 
 
